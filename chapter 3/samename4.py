f2ac6293f83694c1d89628abd71a3d7c512a2210
Jason <jason.ally@gmail.com>
2020-02-27 10:07:26 -0800

    Remove duplicate directories.

--- chapter 3/samename4.py
+++
@@ -1,6 +0,0 @@
-def spam():
-	print(eggs) # Triggers an UnboundLocalError
-	eggs = 'spam local'
-
-eggs = 'global'
-spam()
--- chapter 6/catnapping.py
+++
@@ -1,6 +0,0 @@
-print('''Dear Alice,
-
-Eve's cat has been arrestefd for catnapping, cat burglary, and extortion.
-
-Sincerely,
-Bob''')
