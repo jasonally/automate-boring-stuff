dd3a77757057f97e7ecd236413aeab6e94b1e60f
Jason Ally <jason.ally@gmail.com>
2020-03-22 14:04:43 -0700

    Add proper file open and close handling.

--- chapter_9/regexsearch.py
+++ chapter_9/regexsearch.py
@@ -7,9 +7,12 @@ def file_checker(input_regex):
 	# Make list of every file in current working directory.
 	for file_name in os.listdir('.'):
 		if file_name.endswith('.txt'):
-			for i, line in enumerate(open(file_name)):
+			file = open(file_name, 'r')
+			for i, line in enumerate(file):
 				for match in re.finditer(input_regex, line):
 					print('Found on line %s in %s: %s' % (i+1, file_name, line))
+					print('The matching text: %s' % match.group())
+			file.close()
 
 print('What would you like to search for? Enter a regular expression.')
 regex = re.compile(input())
